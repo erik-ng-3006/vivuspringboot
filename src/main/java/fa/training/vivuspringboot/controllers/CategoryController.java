59b9a1d371cd12f20353f299158cf5fc470c3a13
erik-ng-3006 <erikng3006@gmail.com>
2024-12-10 10:49:05 +0700

    Step 9: Implement delete category

--- src/main/java/fa/training/vivuspringboot/controllers/CategoryController.java
+++ src/main/java/fa/training/vivuspringboot/controllers/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.UUID;
@@ -30,6 +31,24 @@ public class CategoryController {
         CategoryDTO category = categoryService.findById(id);
         model.addAttribute("category", category);
         return "manager/category/detail";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") UUID id, RedirectAttributes redirectAttributes) {
+        boolean isDeleted = false;
+        try {
+            isDeleted = categoryService.delete(id);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Failed to delete category");
+            return "redirect:/manager/category";
+        }
+
+        if (isDeleted) {
+            redirectAttributes.addFlashAttribute("success", "Category deleted successfully");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "Category not found");
+        }
+        return "redirect:/manager/category";
     }
 
 }
